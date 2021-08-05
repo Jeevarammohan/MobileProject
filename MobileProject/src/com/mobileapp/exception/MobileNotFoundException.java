21391ee3efda5f16800700f5e84b14e34bbfa790
Jeeva R <jeevanadar1998@gmail.com>
2021-08-05 23:12:48 +0530

    custome exception class is added

---
+++ MobileProject/src/com/mobileapp/exception/MobileNotFoundException.java
@@ -0,0 +1,20 @@
+package com.mobileapp.exception;
+
+public class MobileNotFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public MobileNotFoundException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public MobileNotFoundException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+}
